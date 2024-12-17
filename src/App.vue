cdf2521a7ca77b9b7043b191e5b8702777ea5b00
Abdujabbor Abdumanonov <Abdujabbor.Abdumanonov@alfainvest.uz>
2024-12-17 14:05:21 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div>
+  <debtor-list>    </debtor-list>
+  </div>
+  </template>
+
+<script >
+import DebtorList from './components/DebtorList.vue'
+export default {
+  components: {DebtorList},
+
+}
+</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/DebtorList.vue
@@ -0,0 +1,61 @@
+<template>
+    <table>
+        <thead> СПИСОК ДЕЙТСВУЮЩИХ ДОЛЖНИКОВ
+            <tr>
+                <th>№</th>
+                <th>ПИФНЛ</th>
+                <th>ФИО</th>
+                <th>Сумма задолженности</th>
+                <th>Сумма погашения</th>
+                <th>Сумма остатка</th>
+                <th>№ договора</th>
+                <th>Продукт</th>
+                <th>Статус</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>1</th>
+                <th>54546453187654</th>
+                <th>ALISHEROV ALISGER ALISHEROVICH</th>
+                <th>100 000 </th>
+                <th>50 000</th>
+                <th>50 000</th>
+                <th>Solfi 524</th>
+                <th>Solfi</th>
+                <th>Претензия</th>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<script>
+export default{}
+</script>
+<style scoped>
+table  {
+    width: 100%;
+    height: 100vh;
+    
+    background-color: red;
+
+}
+
+table thead {
+    border: 1;
+    width: 100vw;
+
+    font-weight: bold;
+}
+
+table tbody {
+    
+    display: block;
+    width: 100vw;
+
+    
+}
+
+table tbody th {
+
+}
+</style>
